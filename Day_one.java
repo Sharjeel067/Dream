ddb734e12d5c575c0f5bbfd4d347a5f0482257ab
Muhammad Sharjeel Ansar <91346510+Sharjeel067@users.noreply.github.com>
2025-09-12 16:19:16 +0500

    Day one of acheiving my Dream

---
+++ Day_one.java
@@ -0,0 +1,49 @@
+/* This is the practice of print and println statements */
+// public class Day_one {
+//     public static void main(String[] args){
+//         System.out.println("Hello World");
+//         System.out.print("This is the first line using the print word.");
+//         System.out.print("This is the second line using the print word which will show the lines in a continous form without line breaking.");
+//         System.out.println("Hello World");
+//     }
+// }
+
+
+/* This is the practice of printing numbers in the print statement */
+// public class Day_one {
+//     public static void main(String[] args){
+//         System.out.println(1);
+//         System.out.println(2 + 1);
+//         System.out.println(3 - 1);
+//         System.out.println(4 * 2);
+//         System.out.println(5 / 5);
+//     }
+// }
+
+/* This is the practice of the variable types */
+// public class Day_one {
+//     public static void main(String[] args){
+//         String name = "Muhammad Sharjeel Ansar";
+//         final int age = 24;
+//         float height = 5.11f;
+//         //height = 6.00f;
+//         char version = 'X';
+//         boolean graduated = true;
+//         System.out.println("My name is: " + name);
+//         System.out.println("My Age is: " + age);
+//         System.out.println("My Height is: " + height);
+//         System.out.println("My DNA version is: " + version);
+//         System.out.println("Am I graduated? " + graduated);
+//     }
+// }
+
+/* This is the practice of the variables manipulation */
+public class Day_one{
+    public static void main (String[] args){
+        var items = 100;
+        float pricePerItem = 9.9f;
+        double totalPrice = items * pricePerItem;
+        char sign = '$';
+        System.out.println("The total Price of the items are: " + totalPrice + sign);
+    }
+}
