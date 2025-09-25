7d07f5b61c024d26169d73e0996a6009c34468f5
Muhammad Sharjeel Ansar <91346510+Sharjeel067@users.noreply.github.com>
2025-09-25 20:31:57 +0500

    two sum problem solved

---
+++ day_seven.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums [i] + nums [j] == target){
+                    return new int [] {i,j};
+                }
+            }
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args){
+        Solution result = new Solution();
+        int[] nums1 = {2,7,11,15};
+        int target = 6;
+
+        System.out.println(result.twoSum(nums1, target));
+    }
+}
